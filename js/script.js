6bf16dd5c14ca8f01448db244893dd1f3634973e
achilles707 <44299519+achilles707@users.noreply.github.com>
2021-02-09 13:40:51 -0800

    meets expectations i think

--- js/script.js
+++ js/script.js
@@ -3,15 +3,11 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-//console.log(data);
-
 
 /*
 Create the `showPage` function
@@ -19,22 +15,17 @@ This function will create and insert/append the elements needed to display a "pa
 */
 function showPage(list, page) {
 
-   // displays 9 students per page
-   let itemsPerPage = 9;
-
-   // create two variables which will represent the index for the first and last student on the page
-   let startIndex = (page * itemsPerPage) - itemsPerPage;  // items per page doesn't exist yet
-   let endIndex = page * itemsPerPage;
-  // select the element with a class of `student-list` and assign it to a variable
-   let studentList =  document.getElementsByClassName('student-list');
-  // set the innerHTML property of the variable you just created to an empty string
+   const itemsPerPage = 9;
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   const studentList =  document.getElementsByClassName('student-list')[0];
    studentList.innerHTML = "";
-  // loop over the length of the `list` parameter
+   
+   //  creates the studentItem to be displayed
    for(let i=0; i < list.length; i++) {
-      // inside the loop create a conditional to display the proper students
+
       if (i >= startIndex && i < endIndex) {
-      // inside the conditional:
-         // create the elements needed to display the student information
+         // build student element
          let studentItem = `
             <li class="student cf">
                <div class="student-details">
@@ -47,22 +38,59 @@ function showPage(list, page) {
                </div>
             </li>
          `;   
-         console.log(studentList);
 
-         //console.log(studentItem);
-         studentList.insertAdjacentHTML('beforeend', 'studentItem');
+         // adds each studentItem to the page 
+         studentList.insertAdjacentHTML('beforeend', studentItem);
       }
-
    }
 }
-// test call
-console.log(showPage(data, 1));
-showPage(data, 1);
+
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+   // vars for pagination
+   const pages = list.length / 9;
+   const linkList = document.querySelector('.link-list');   
+   linkList.innerHTML = '';
+   let active = '';
 
+   // create button element
+   for(let i=1; i<=pages; i++) {
+      let buttonElement = `
+         <li>
+            <button type='button'>${i}</button>
+         </li>
+      `;
+      
+      // add buttons to the page
+      linkList.insertAdjacentHTML('beforeend', buttonElement);
+   }
 
+   console.log(linkList);
+   // set first button as active 
+   active = document.querySelector('button');
+   active.className = 'active';
+
+   // adds the active class to the button that is clicked 
+   //    and displays the correct page
+   linkList.addEventListener('click', (event) => {
+      // move active class to the correct button
+      if(event.target.tagName == 'BUTTON') {
+         let activeButton = document.querySelector('.active');
+         activeButton.classList.remove('active');
+         event.target.className = 'active';
+         let page = event.target.textContent;
+
+         // displays studentItems for each page
+         showPage(list, page);
+      }
+   });
+
+}
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
